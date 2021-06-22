1926586aec2aeffba4bea599ef64a2cd2091bfcb
rodrigo-represa <rodrigo.represa@mediamonks.com>
2021-06-23 01:35:01 +0200

    Start testing

--- app/src/androidTest/java/com/represa/adidas/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/represa/adidas/ExampleInstrumentedTest.kt
@@ -1,24 +1,58 @@
 package com.represa.adidas
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.represa.adidas.ui.viewmodels.ProductViewModel
 import org.junit.Test
+import org.junit.Rule
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.represa.adidas.di.appModule
+import com.represa.adidas.di.useCaseModule
+import com.represa.adidas.ui.viewmodels.ProductDetailViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.After
+import org.junit.Before
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import org.koin.core.context.loadKoinModules
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ExampleInstrumentedTest: KoinTest {
+
+    private val productViewModel by inject<ProductViewModel>()
+    private val productDetailViewModel by inject<ProductDetailViewModel>()
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+
+    @Before
+    fun before() {
+        loadKoinModules(listOf(appModule, useCaseModule))
+        productViewModel.populateDatabase()
+    }
+
+    @ExperimentalCoroutinesApi
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.represa.adidas", appContext.packageName)
+    fun addition_isCorrectt() {
+        val testContextProvider = TestContextProvider()
+        //productDetailViewModel.changeContextProvider(testContextProvider)
+
+        productDetailViewModel.getProduct("HI334")
+
+
+        val value = productDetailViewModel.product.getOrAwaitValue()
+
+
+        assert(value.id == "HI334")
     }
 }
